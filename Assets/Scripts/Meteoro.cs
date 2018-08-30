c9a4088d73668922a28e18afa6ef1e9fc92181d3
WagnerCardozo <wagnercardozo.profissional@gmail.com>
2018-08-29 21:23:05 -0300

    Initial commit

---
+++ Assets/Scripts/Meteoro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteoro : MonoBehaviour {
+	
+	private Rigidbody2D meteoroRb;
+	private int atrito;
+	public int atritoMax;
+	public Vector3 posicao;
+	public GameObject meteoroPrefab;
+	
+		
+		void Start () {
+		
+		meteoroRb = GetComponent<Rigidbody2D>();
+		atrito = Random.Range(2, atritoMax);
+		meteoroRb.drag = atrito;
+		
+		
+		posicao = transform.position;
+	}
+		
+	void OnBecameInvisible()
+	{
+		Instantiate(meteoroPrefab, posicao, transform.localRotation);
+		Pontuacao.pontos += 1;
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D playerRb;
+	public float velocidade = 0;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		playerRb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetMouseButtonDown(0)){
+			
+			velocidade = velocidade * -1;
+			
+		}
+		
+		if(velocidade < 0){
+			
+			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		}
+		
+		if(velocidade > 0){
+			
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+			
+		}
+		
+		playerRb.velocity = new Vector2 (velocidade, playerRb.velocity.y);
+		
+
+	}
+	
+	void OnTriggerEnter2D(Collider2D colisao){
+		if(colisao.gameObject.tag == "meteoro"){
+			SceneManager.LoadScene("gameover");
+		}
+	}
+}
